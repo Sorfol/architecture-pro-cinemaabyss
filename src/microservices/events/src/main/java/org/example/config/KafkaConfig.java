6718cac5b83e1a11955408d4df609c1ea6a3843a
Sorfol <ger-rodin@yandex.ru>
2025-07-03 22:09:12 +0300

    fixing event service

--- src/microservices/events/src/main/java/org/example/config/KafkaConfig.java
+++ src/microservices/events/src/main/java/org/example/config/KafkaConfig.java
@@ -1,6 +1,8 @@
 package org.example.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
@@ -19,9 +21,9 @@ public class KafkaConfig {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
         return props;
     }
 
--- src/microservices/events/src/main/java/org/example/service/EventProducerService.java
+++ src/microservices/events/src/main/java/org/example/service/EventProducerService.java
@@ -1,6 +1,5 @@
 package org.example.service;
 
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,25 +8,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EventProducerService {
-
     private static final Logger logger = LoggerFactory.getLogger(EventProducerService.class);
-
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public EventProducerService(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
     public <T> void sendMessage(String topicName, T message) {
         try {
-            kafkaTemplate.send(topicName, message.toString());
+            kafkaTemplate.send(topicName, message); // Просто отправляем объект
             logger.info("Отправлено сообщение в тему {}: {}", topicName, message);
         } catch (Exception e) {
             logger.error("Ошибка отправки сообщения:", e);
-            throw new RuntimeException("Failed to send message", e);
+            throw new RuntimeException("Failed to send message to Kafka", e);
         }
     }
 }
