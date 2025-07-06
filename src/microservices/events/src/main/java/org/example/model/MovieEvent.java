64618d8f03daa0f22adc591d4881032379fe1071
Sorfol <ger-rodin@yandex.ru>
2025-07-06 19:59:07 +0300

    fixing event service

--- src/microservices/events/src/main/java/org/example/model/MovieEvent.java
+++ src/microservices/events/src/main/java/org/example/model/MovieEvent.java
@@ -1,6 +1,5 @@
 package org.example.model;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
